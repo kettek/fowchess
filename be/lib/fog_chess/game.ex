fae808a3eb59cbe8a8edec8022ac9f1295c2f48b
kts of kettek <kts@kettek.net>
2022-09-21 04:55:15 -0700

    Adjust logic lightly

--- be/lib/fog_chess/game.ex
+++ be/lib/fog_chess/game.ex
@@ -1,4 +1,21 @@
-defmodule Game do
+defmodule FogChess.Cell do
+  @enforce_keys [:x, :y]
+  defstruct [:x, :y]
+end
+
+defmodule FogChess.Game do
   @enforce_keys [:uuid]
-  defstruct [:uuid, connections: []]
+  defstruct [:uuid, cells: %{}, players: %{}, turn: :white]
+
+  @spec check_player(String.t()) :: none
+  def check_player(player_uuid) do
+    Map.has_key?(:players, player_uuid)
+  end
+
+  def players_turn(player_color) do
+    case :turn do
+      player_color -> {:ok, player_color}
+      _ -> {:error, :wrong_turn}
+    end
+  end
 end
