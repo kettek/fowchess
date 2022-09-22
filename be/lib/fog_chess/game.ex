2d11635c5de319d705eac56261cf9addc9264af4
kts of kettek <kts@kettek.net>
2022-09-21 18:34:36 -0700

    Implement game creation

--- be/lib/fog_chess/game.ex
+++ be/lib/fog_chess/game.ex
@@ -3,19 +3,47 @@ defmodule FogChess.Cell do
   defstruct [:x, :y]
 end
 
+defmodule FogChess.PlayerConn do
+  defstruct [:conn_pid]
+
+  def send_update(payload) do
+    send(:conn_pid, {:update, payload})
+  end
+end
+
 defmodule FogChess.Game do
-  @enforce_keys [:uuid]
-  defstruct [:uuid, cells: %{}, players: %{}, turn: :white]
+  use Agent
 
-  @spec check_player(String.t()) :: none
-  def check_player(player_uuid) do
-    Map.has_key?(:players, player_uuid)
+  defstruct [:uuid, :pid, cells: %{}, players: %{}, turn: :white]
+
+  def start_link(game) do
+    Agent.start_link(fn -> game end)
   end
 
-  def players_turn(player_color) do
-    case :turn do
-      player_color -> {:ok, player_color}
-      _ -> {:error, :wrong_turn}
-    end
+  def set_pid(pid) do
+    Agent.update(pid, fn(value) -> %{value | pid: pid} end)
   end
+
+  def get(pid) do
+    Agent.get(pid, fn(value) -> value end)
+  end
+
+  def put_player(pid, uuid, player_conn) do
+    Agent.update(pid, fn(value) -> %{value | players: Enum.into(value.players, %{uuid => player_conn})} end)
+  end
+
+  def delete_player(pid, uuid) do
+    Agent.update(pid, fn(value) -> %{value | players: Map.delete(value.players, uuid) } end)
+  end
+
+  #def check_player(pid, player_uuid) do
+  #  Agent.get(pid, &Map.get(&1.players, player_uuid))
+  #end
+
+  #def players_turn(pid, player_color) do
+  #  case Agent.get(pid, :turn) do
+  #    player_color -> {:ok, player_color}
+  #    _ -> {:error, :wrong_turn}
+  #  end
+  #end
 end
--- be/lib/fog_chess/server.ex
+++ be/lib/fog_chess/server.ex
@@ -35,9 +35,10 @@ defmodule FogChess.HttpRouter do
   end
 
   post "/game/create" do
-    Logger.debug(conn.body_params)
     uuid = UUID.uuid4()
-    FogChess.Games.put(uuid, %FogChess.Game{uuid: uuid})
+    {:ok, pid} = FogChess.Game.start_link(%FogChess.Game{uuid: uuid})
+    FogChess.Game.set_pid(pid)
+    FogChess.Games.put(uuid, pid)
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(%{"ok" => "a new kingdom!", "id" => uuid}))
@@ -45,37 +46,87 @@ defmodule FogChess.HttpRouter do
 
   get "/games/:id" do
     id = Map.get(conn.params, "id")
-    IO.inspect(FogChess.Games.get(id))
-    send_resp(conn, 200, "TODO: query for game, make it if it doesn't, then send game info along with a session key for the client")
+    gamePid = FogChess.Games.get(id)
+    game = FogChess.Game.get(gamePid)
+    case game do
+      nil ->
+        conn
+        |> send_resp(404, Jason.encode!(%{"nok" => "no such game"}))
+        _ ->
+        conn
+        |> send_resp(200, Jason.encode!(%{"ok" => "it exists!"}))
+    end
+  end
+
+  put "/games/:id/move" do
+    conn
+    |> send_resp(404, Jason.encode!(%{"nok" => "invalid"}))
   end
 
   get "/games/:id/stream" do
-    conn
-    |> put_resp_content_type("text/event-stream")
-    |> put_resp_header("connection", "keep-alive")
-    |> put_resp_header("cache-control", "no-cache")
-    |> send_chunked(200)
-    |> stream_loop(0)
+    player_conn = %FogChess.PlayerConn{
+      conn_pid: self(),
+    }
+    player_uuid = Map.get(conn.query_params, "id")
+    if player_uuid == nil do
+      conn
+      |> send_resp(401, "missing player id")
+    else
+      id = Map.get(conn.params, "id")
+      game_pid = FogChess.Games.get(id)
+      case game_pid do
+        nil ->
+          conn
+          |> send_resp(404, "invalid game")
+          _ ->
+          FogChess.Game.put_player(game_pid, player_uuid, player_conn)
+          conn
+          |> put_resp_content_type("text/event-stream")
+          |> put_resp_header("connection", "keep-alive")
+          |> put_resp_header("cache-control", "no-cache")
+          |> send_chunked(200)
+          |> stream_loop(stream_watcher(self(), game_pid, player_uuid))
+      end
+    end
   end
 
-  defp stream_loop(conn, it) do
-    case it do
-      5 ->
-        payload = Jason.encode!(%{"msg" => "bye bye on message #{it}"})
-        Plug.Conn.chunk(conn, "id: #{it}\nevent: exit\ndata: #{payload}}\n\n")
-        conn
-      _ ->
-        receive do
-          {:move} ->
-            payload = Jason.encode!(%{"msg" => "secret message from beyond"})
-            Plug.Conn.chunk(conn, "id: #{it}\ndata: #{payload}\n\n")
-            stream_loop(conn, it+1)
-          after 1_000 ->
-            payload = Jason.encode!(%{"msg" => "this is message ##{it}"})
-            Plug.Conn.chunk(conn, "id: #{it}\ndata: #{payload}\n\n")
-            stream_loop(conn, it+1)
-          end
+  defp stream_watcher(target, game_pid, player_uuid) do
+    spawn(fn -> stream_watcher_loop(target, game_pid, player_uuid) end)
+  end
+
+  defp stream_watcher_loop(target, game_pid, player_uuid) do
+    receive do
+      {:tick} ->
+        stream_watcher_loop(target, game_pid, player_uuid)
+      after 2_000 ->
+        FogChess.Game.delete_player(game_pid, player_uuid)
     end
+  end
+
+  defp stream_loop(conn, watcher_pid) do
+    send(watcher_pid, {:tick})
+    receive do
+      {:update} ->
+        Logger.info("got update")
+        send(watcher_pid, {:tick})
+        stream_loop(conn, watcher_pid)
+      {:move} ->
+        payload = Jason.encode!(%{"msg" => "secret message from beyond"})
+        Plug.Conn.chunk(conn, "data: #{payload}\n\n")
+        stream_loop(conn, watcher_pid)
+      after 1_000 ->
+        send(watcher_pid, {:tick})
+        stream_loop(conn, watcher_pid)
+        #payload = Jason.encode!(%{"msg" => "this is message ##{it}"})
+        #case Plug.Conn.chunk(conn, "id: #{it}\ndata: #{payload}\n\n") do
+        #  {:ok, conn} ->
+        #    Logger.info("continue")
+        #    stream_loop(conn, watcher_pid, it+1)
+        #  {:error, reason} ->
+        #    Logger.info(reason)
+        #    {:halt, conn}
+        #end
+      end
   end
 
   # 404s, of course
